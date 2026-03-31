47a1feb50bd8298d217d9dd3e6d6f626fe85778e
Iker <iker.vera.52898@ikasle.egibide.org>
2026-03-31 09:46:37 +0200

    Creación de Clases en Modelo

---
+++ Programacion/AppEsports/src/main/java/org/example/appesports/Modelo/Admin.java
@@ -0,0 +1,11 @@
+package org.example.appesports.Modelo;
+
+public class Admin extends Usuario {
+
+    public Admin() {
+    }
+
+    public Admin(int idUsuario, String nombreUsuario, String contrasena) {
+        super(idUsuario, nombreUsuario, contrasena);
+    }
+}

c6875184b1896d9790ef0400913f90afe366cd41
Unai <unai.castellano.47462@ikasle.egibide.org>
2026-04-20 11:38:27 +0200

    JUNIT de Usuarios

--- Programacion/AppEsports/src/main/java/org/example/appesports/Utilidades/ValidarDatos.java
+++ Programacion/AppEsports/src/main/java/org/example/appesports/Utilidades/ValidarDatos.java
@@ -37,7 +37,4 @@ public class ValidarDatos {
             throw new Exception("Sueldo no valido");
         }
     }
-
-
-
 }
---
+++ Programacion/AppEsports/src/test/java/org/example/appesports/Utilidades/ValidarDatosTest.java
@@ -0,0 +1,24 @@
+package org.example.appesports.Utilidades;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidarDatosTest {
+    @org.junit.jupiter.api.Test
+    @Order(1)
+    void validarUsername() {
+        // Caso 1: El nombre "admin" cumple con el patrón ^[A-Za-z0-9]+$
+        assertTrue("admin".matches("^[A-Za-z0-9]+$"));
+
+        // Caso 2: El nombre "admin123" cumple con el patrón
+        assertTrue("admin123".matches("^[A-Za-z0-9]+$"));
+
+        // Caso 3: Un nombre con espacios NO cumple con el patrón
+        assertFalse("admin login".matches("^[A-Za-z0-9]+$"));
+
+        // Caso 4: Un nombre con caracteres especiales NO cumple con el patrón
+        assertFalse("admin_2024".matches("^[A-Za-z0-9]+$"));
+    }
+}
